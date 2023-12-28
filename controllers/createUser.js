5ac7c73a6bd098e4420f75c83482a5c7f5c6f96d
Addy-codes <adeeb9839@gmail.com>
2023-12-28 12:15:58 +0530

    Initial Commit

---
+++ controllers/createUser.js
@@ -0,0 +1,39 @@
+const User = require('../models/userSchema');
+
+exports.createUser = async (req, res) => {
+    try {
+        const { name, email, phone } = req.body;
+        const checkUser = await User.findOne({ email });
+        if (checkUser) {
+            res
+                .status(200)
+                .json({
+                    success: true,
+                    message: 'User Exists',
+                    userId: checkUser._id
+                })
+        }
+        else {
+            let createEntry;
+            phone ?
+             createEntry = await User.create({ name, email, phone })
+             :
+             createEntry= await User.create({ name, email })
+            res
+                .status(200)
+                .json({
+                    success: true,
+                    message: 'User Successfully Added',
+                    userId: createEntry._id
+                })
+        }
+    }
+    catch (error) {
+        res
+            .status(500)
+            .json({
+                success: false,
+                message: `Internal Server Error ${error.message}`
+            })
+    }
+}
